9c0ddd6331adbad2feacdeb7076a6f70808fe0b3
Reinhard Tartler <siretart@tauware.de>
2011-11-04 06:11:08 +0100

    Imported Upstream version 1.0.15

--- va/android/va_android.cpp
+++ va/android/va_android.cpp
@@ -25,6 +25,8 @@
 #define _GNU_SOURCE 1
 #include "va.h"
 #include "va_backend.h"
+#include "va_trace.h"
+#include "va_fool.h"
 #include "va_android.h"
 #include "va_dricommon.h" /* needs some helper functions from this file */
 #include <stdio.h>
@@ -123,7 +125,7 @@ static VAStatus va_DisplayContextGetDriverName (
     };
 
     memset(dri_state, 0, sizeof(*dri_state));
-    dri_state->fd = open_device(DEVICE_NAME);
+    dri_state->fd = open_device((char *)DEVICE_NAME);
     
     if (dri_state->fd < 0) {
         fprintf(stderr,"can't open DRM devices\n");
@@ -267,11 +269,6 @@ extern "C"  {
         );
 }
 
-#define VA_TRACE(trace_func,...)                \
-    if (trace_flag) {                           \
-        trace_func(__VA_ARGS__);                \
-    }
-
 VAStatus vaPutSurface (
     VADisplay dpy,
     VASurfaceID surface,
@@ -300,9 +297,9 @@ VAStatus vaPutSurface (
     CHECK_DISPLAY(dpy);
     ctx = CTX(dpy);
 
-    VA_TRACE(va_TracePutSurface, dpy, surface, static_cast<void*>(&draw), srcx, srcy, srcw, srch,
-             destx, desty, destw, desth,
-             cliprects, number_cliprects, flags );
+    VA_TRACE_LOG(va_TracePutSurface, dpy, surface, static_cast<void*>(&draw), srcx, srcy, srcw, srch,
+                 destx, desty, destw, desth,
+                 cliprects, number_cliprects, flags );
     
     return ctx->vtable->vaPutSurface( ctx, surface, static_cast<void*>(&draw), srcx, srcy, srcw, srch, 
                                      destx, desty, destw, desth,
